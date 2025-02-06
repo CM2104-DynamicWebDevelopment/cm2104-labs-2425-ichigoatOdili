5732188a6e90545f2d163d6b0228b22f8ef8bc9c
Odili Tobechukwu <t.odili@rgu.ac.uk>
2025-02-06 11:48:02 +0000

    lab

--- lab_11/server.js
+++ lab_11/server.js
@@ -13,4 +13,36 @@ app.get('/test', function(req, res){
     var y = req.query.y;
     res.send("x + y="+(x+y));
    });
+
+   app.get('/calc', function(req, res){
+    var X = parseFloat(req.query.X);
+    var Y = parseFloat(req.query.Y);
+    const operator = req.query.operator; 
+
+    if(isNaN(X) || isNaN(Y)) {
+        return res.status(400).send('Invalid number provided');
+     }
+
+     let result;
+
+     switch(operator) {
+        case 'add':
+            result= X + Y;
+            break;
+        case 'multiply':
+            result= X * Y;
+            break;
+        case 'subtract':
+            result = X - Y;
+            break;
+        case 'divide':
+            result = Y !== 0 ? X / Y: 'Error: Division by zero';
+            break;    
+         
+        default:
+            return res.status(400).send('Invalid operator. Use add, suntrsct, multiply, divide.');    
+
+
+     }
+   })
 app.listen(8080);
